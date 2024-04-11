6947e469446ad24c752d83ae9de06e934f90a920
Pscolovski <157747460+Pscolovski@users.noreply.github.com>
2024-04-11 11:25:54 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let bitNumberOne = 10;
+let bitNumberTwo = 20;
+
+console.log(bitNumberOne & bitNumberTwo);
+console.log(bitNumberOne | bitNumberTwo);
+console.log(~bitNumberTwo);
+console.log(bitNumberOne ^ bitNumberTwo);
+console.log(bitNumberOne << 1);
+console.log(bitNumberOne << 5);
+console.log(bitNumberOne >> 1);
+console.log(bitNumberOne >>> 1);
+console.log((-100 >>> 3).toString(2));
+console.log(true << 1);
+console.log(false << 1);
+console.log(true >> 1);
+console.log(false >> 1);
+console.log(~false);
+console.log(~true);
